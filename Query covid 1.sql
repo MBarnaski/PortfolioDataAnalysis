8d1e3af9624595d7b82dbfcedf6386f21451b410
MBarnaski <mariaritabarnaski@gmail.com>
2024-05-21 13:15:21 -0300

    Update Query covid 1.sql

--- Query covid 1.sql
+++ Query covid 1.sql
@@ -1,57 +1,57 @@
---select * from PortfolioProjetos.dbo.['owid-covid-deaths']
---select * from PortfolioProjetos.dbo.['owid-covid-vaccines']
---select location, date, total_cases, new_cases, total_deaths, population from PortfolioProjetos.dbo.['owid-covid-deaths']
---where continent is not null
---order by 1,2
+select * from PortfolioProjetos.dbo.['owid-covid-deaths']
+select * from PortfolioProjetos.dbo.['owid-covid-vaccines']
+select location, date, total_cases, new_cases, total_deaths, population from PortfolioProjetos.dbo.['owid-covid-deaths']
+where continent is not null
+order by 1,2
 
----- Casos vs Mortes, probabilidade de morte após infecção por país
-----select location, date, total_cases, total_deaths, (cast(total_deaths as float)/cast(total_cases as float))*100  as PorcentagemMortes 
-----from PortfolioProjetos.dbo.['owid-covid-deaths']
-----where location like '%brazil%'
-----order by 1,2
+---- Casos vs Mortes, probabilidade de morte apÃ³s infecÃ§Ã£o por paÃ­s
+select location, date, total_cases, total_deaths, (cast(total_deaths as float)/cast(total_cases as float))*100  as PorcentagemMortes 
+from PortfolioProjetos.dbo.['owid-covid-deaths']
+where location like '%brazil%'
+order by 1,2
 
-------Casos totais vs população, porcentagem da populçao que contraiu covid
-----select location, date, total_cases, population, (cast(total_cases as float)/population)*100  as PorcentagemInfeccoes 
-----from PortfolioProjetos.dbo.['owid-covid-deaths']
+------Casos totais vs populaÃ§Ã£o, porcentagem da populÃ§ao que contraiu covid
+select location, date, total_cases, population, (cast(total_cases as float)/population)*100  as PorcentagemInfeccoes 
+from PortfolioProjetos.dbo.['owid-covid-deaths']
 ------where location like '%brazil%' and continent is not null
-----order by 1,2
+order by 1,2
 
-----Países com maior taxa de infecção em relação a população
-----select location, population, max(cast (total_cases as float)) as MaiorTaxaInfeccao, max(cast(total_cases as float)/population)*100  as PorcentagemInfeccoes 
-----from PortfolioProjetos.dbo.['owid-covid-deaths']
+----PaÃ­ses com maior taxa de infecÃ§Ã£o em relaÃ§Ã£o a populaÃ§Ã£o
+select location, population, max(cast (total_cases as float)) as MaiorTaxaInfeccao, max(cast(total_cases as float)/population)*100  as PorcentagemInfeccoes 
+from PortfolioProjetos.dbo.['owid-covid-deaths']
 ------where location like '%brazil%' and continent is not null
-----group by location, population
-----order by PorcentagemInfeccoes desc
+group by location, population
+order by PorcentagemInfeccoes desc
 
-----Países com maior taxa de mortes em relação a população
-----select location, max(cast (total_deaths as int)) as MaiorTaxaMortes
-----from PortfolioProjetos.dbo.['owid-covid-deaths']
+----PaÃ­ses com maior taxa de mortes em relaÃ§Ã£o a populaÃ§Ã£o
+select location, max(cast (total_deaths as int)) as MaiorTaxaMortes
+from PortfolioProjetos.dbo.['owid-covid-deaths']
 ------where location like '%brazil%'
-----where  continent is not null
-----group by PortfolioProjetos.dbo.['owid-covid-deaths'].location 
-----order by MaiorTaxaMortes desc
+where  continent is not null
+group by PortfolioProjetos.dbo.['owid-covid-deaths'].location 
+order by MaiorTaxaMortes desc
 
 ----Por continente
---select location, max(cast (total_deaths as int)) as MaiorTaxaMortes
---from PortfolioProjetos.dbo.['owid-covid-deaths']
-----where location like '%brazil%'
---Where continent is not null
---group by location
---order by MaiorTaxaMortes desc
+select location, max(cast (total_deaths as int)) as MaiorTaxaMortes
+from PortfolioProjetos.dbo.['owid-covid-deaths']
+--where location like '%brazil%'
+Where continent is not null
+group by location
+order by MaiorTaxaMortes desc
 
-----Números totais globais
---select sum(new_cases) as TotalDeCasos, sum(new_deaths) TotalDeMortes, SUM(new_deaths)/NULLIF(sum(new_cases),0)*100
---from PortfolioProjetos.dbo.['owid-covid-deaths']
+----NÃºmeros totais globais
+select sum(new_cases) as TotalDeCasos, sum(new_deaths) TotalDeMortes, SUM(new_deaths)/NULLIF(sum(new_cases),0)*100
+from PortfolioProjetos.dbo.['owid-covid-deaths']
 ----where location like '%brazil%'
---where continent is not null
-----group by date
---order by 1,2
+where continent is not null
+group by date
+order by 1,2
 
 --join
 select * from PortfolioProjetos.dbo.['owid-covid-deaths'] as mortes join PortfolioProjetos.dbo.['owid-covid-vaccines'] as vac
 on mortes.location = vac.location and mortes.date=vac.date
 
---Total de população vs Vacinados
+--Total de populaÃ§Ã£o vs Vacinados
 --cte
 with PopVsCont (continent, Location, Date, Population, new_vaccinations, PessoasVacinadas)
 as(
@@ -65,8 +65,8 @@ where mortes.continent is not null
 select *, (PessoasVacinadas/Population)*100 from PopVsCont 
 
 --temp table
-drop table if exists #tempPorcentagemPopulaçãoVacinada
-create table #tempPorcentagemPopulaçãoVacinada(
+drop table if exists #tempPorcentagemPopulaÃ§Ã£oVacinada
+create table #tempPorcentagemPopulaÃ§Ã£oVacinada(
 continent nvarchar (255),
 location nvarchar (255),
 data datetime,
@@ -74,14 +74,14 @@ population real,
 new_vaccination real,
 PessoasVacinadas real
 )
-insert into #tempPorcentagemPopulaçãoVacinada
+insert into #tempPorcentagemPopulaÃ§Ã£oVacinada
 select mortes.continent, mortes.location, mortes.date, mortes.population, vac.new_vaccinations, 
 sum(convert(real, vac.new_vaccinations)) over (partition by mortes.location order by mortes.location,mortes.date)
 PessoasVacinadas
 from PortfolioProjetos.dbo.['owid-covid-deaths'] as mortes join PortfolioProjetos.dbo.['owid-covid-vaccines'] as vac
 on mortes.location = vac.location and mortes.date=vac.date
 --where mortes.continent is not null
-select *, (PessoasVacinadas/Population)*100 from #tempPorcentagemPopulaçãoVacinada 
+select *, (PessoasVacinadas/Population)*100 from #tempPorcentagemPopulaÃ§Ã£oVacinada 
 
 --criar view para consultas futuras
 create view PorcentagemPopulacaoVacinada as
